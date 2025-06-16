5298286a9824d4976219b646f8a08aa80aac9e07
gleversonrafael <gleverson_rafael@hotmail.com>
2025-06-16 11:59:56 -0300

    Add files via upload

---
+++ 16-06-t/index.js
@@ -0,0 +1,13 @@
+const a = "Carlos";
+const b = "Locars";
+
+console.log(isAnagram(a, b));
+
+function isAnagram(stringA, stringB) {
+    let equalToAB = 0;
+    
+    for(let current = 0; current < stringA.length; current++) {
+        if(current)
+
+    }
+}
